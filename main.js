8ce49c5f4b52768c2a54548f7a9b4a850d331bfd
Irina Kats <irina8kats@gmail.com>
2024-06-29 21:14:39 -0400

    fix: update text

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello!</h1>
-      <currency-table currency="eur"></currency-table>
-    </div>
+    <h1>Table of Currencies</h1>
+      <currency-table currency="usd"></currency-table>
+    <div>Thank you for reviewing this demonstration of a web component</div>
   </div>
-`
-
+`;
